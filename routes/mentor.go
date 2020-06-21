19d458902159e19aff28be47d28e280e735f0464
Palkit Lohia <57593495+spookbite@users.noreply.github.com>
2020-06-21 17:19:24 +0530

    completed the mentorReg function

--- routes/mentor.go
+++ routes/mentor.go
@@ -3,8 +3,45 @@ package routes
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
+	"io/ioutil"
+	"github.com/jinzhu/gorm"
+	"kwoc20-backend/models"
 )
 
 func MentorReg(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello")
+
+	var mentor models.Mentor
+	body, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(body, &mentor)
+	if err != nil {
+	  http.Error(w, err.Error(), 400)
+	  return 		
+	}
+
+	
+	db, err := gorm.Open("sqlite3", "kwoc.db")
+	if err != nil {
+		 http.Error(w, err.Error(), 500)
+		 return
+	}
+	defer db.Close()
+
+	err = db.Create(&models.Mentor{
+				Name: Mentor.Name,
+				email: Mentor.Email,
+				github_handle: project.git_han,
+				access_token: project.acc_tok,
+			}).Error;
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "success"}`))
+
 }
+
