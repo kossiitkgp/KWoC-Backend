20af6d05cfb7e48b6364f275fee1256e76b21472
Shubham Mishra <smishra99.iitkgp@gmail.com>
2020-11-25 02:16:33 +0530

    CORS Unscrew 3

--- utils/permissivecors.go
+++ utils/permissivecors.go
@@ -12,5 +12,5 @@ func PermissiveCORS(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Bearer")
 }
