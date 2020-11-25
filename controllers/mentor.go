999cfa229ec647380e245baee0d753d7f48ca7d6
rakaar <rka87338@gmail.com>
2020-11-25 18:14:40 +0530

    Added Mentor Dashboard endpointd endpoints

--- controllers/mentor.go
+++ controllers/mentor.go
@@ -26,22 +26,29 @@ func MentorReg(req map[string]interface{}, r *http.Request) (interface{}, int) {
 }
 
 func MentorDashboard(req map[string]interface{}, r *http.Request) (interface{}, int){
-	// return "name", 200
 	username := req["username"].(string)
 
+	
 	mentor := models.Mentor{}
 	db := utils.GetDB()
 	defer db.Close()
-	db.Where("name = ?", username).First(&mentor)
-
+	db.Where(&models.Mentor{Username: username}).First(&mentor)
 	if mentor.ID == 0 {
 		return "no user", 400
 	}
+	
+	var projects []models.Project
+	db.Where("mentor_id = ?", mentor.ID).Find(&projects)
+	// projects_json, err := json.Marshal(projects)
+	
+	// var projects []models.Project
+	// projects := models.Project{}
+	// db.Where(&models.Project{MentorID: mentor.ID}).First(&projects)
 
 	type Response map[string]interface{}
 	res := Response{
-		"username" : username,
 		"name": mentor.Name,
+		"projects": projects,
 	}
 
 	return res, 200
