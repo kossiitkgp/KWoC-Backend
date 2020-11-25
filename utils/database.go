5fae3037873478ac4de0d83a69fa0d452d04cfe7
Mukul Mehta <mukul.csiitkgp@gmail.com>
2020-11-26 01:26:16 +0530

    Update database to MySQL

--- utils/database.go
+++ utils/database.go
@@ -5,16 +5,32 @@ import (
 	"os"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql" // MySQL Dialect
 
 	"kwoc20-backend/models"
 )
 
+// InitialMigration Initialize migration
 func InitialMigration() {
-	db, err := gorm.Open("sqlite3", "kwoc.db")
+	DatabaseUsername := os.Getenv("DATABASE_USERNAME")
+	DatabasePassword := os.Getenv("DATABASE_PASSWORD")
+	DatabaseName := os.Getenv("DATABASE_NAME")
+	DatabaseHost := os.Getenv("DATABASE_HOST")
+	DatabasePort := os.Getenv("DATABASE_PORT")
+
+	DatabaseURI := fmt.Sprintf(
+		"%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		DatabaseUsername,
+		DatabasePassword,
+		DatabaseHost,
+		DatabasePort,
+		DatabaseName,
+	)
+
+	db, err := gorm.Open("mysql", DatabaseURI)
 	if err != nil {
-		fmt.Println(err.Error())
-		panic("failed to connect database")
+		LOG.Println(err)
+		panic(err)
 	}
 	defer db.Close()
 
@@ -23,6 +39,7 @@ func InitialMigration() {
 	db.AutoMigrate(&models.Student{})
 }
 
+// GetDB Get Database
 func GetDB() *gorm.DB {
 	DatabaseUsername := os.Getenv("DATABASE_USERNAME")
 	DatabasePassword := os.Getenv("DATABASE_PASSWORD")
