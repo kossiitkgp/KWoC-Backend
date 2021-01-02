266d555df4bddf3e929c5b5f01d977de6377c202
rakaar <rka87338@gmail.com>
2021-01-02 11:32:54 +0530

    Added code for endevals

--- routes/student.go
+++ routes/student.go
@@ -13,6 +13,7 @@ import (
 func RegisterStudent(r *mux.Router) {
 	r.HandleFunc("/form", utils.PermissiveCORSMiddleware(utils.LoginRequired(utils.JsonIO(controllers.StudentReg)))).Methods("POST")
 	r.HandleFunc("/dashboard", utils.PermissiveCORSMiddleware(utils.JsonIO(controllers.StudentDashboard))).Methods("POST")
+	r.HandleFunc("/bloglink", utils.PermissiveCORSMiddleware(utils.LoginRequired(utils.JsonIO(controllers.StudentBlogLink)))).Methods("POST")
 	// r.HandleFunc("/form", utils.JsonIO(controllers.StudentReg)).Methods("POST")
 
 }
