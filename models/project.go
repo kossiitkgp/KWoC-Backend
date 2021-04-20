d558e9db2038084df935539ae682e73b86cd57d6
rakaar <rka87338@gmail.com>
2021-04-20 20:01:41 +0530

    Added PR and Commits Model

--- models/project.go
+++ models/project.go
@@ -17,6 +17,7 @@ type Mentor struct {
 //Project Model
 type Project struct {
 	gorm.Model
+	ID 				  uint
 	Name              string
 	Desc              string `gorm:"size:2550"`
 	Tags              string
@@ -25,6 +26,31 @@ type Project struct {
 	Branch            string
 	README            string `gorm:"size:255000000"`
 	ProjectStatus     bool
-	Mentor         	  Mentor
-	SecondaryMentor   Mentor
+	Mentor         	  Mentor // foreign key
+	SecondaryMentor   Mentor // foreign key
+}
+
+// Commits Model
+type Commits struct{
+	gorm.Model
+	ID uint
+	URL string
+	Message string
+	LinesAdded uint
+	LinesRemoved uint
+	
+	Project Project // foreign key
+	Student Student // foreign key
+}
+
+// PRs Model
+type PullRequest struct {
+	gorm.Model
+	ID	uint
+	URL string
+	Title string
+	
+	Project Project //foreign key
+	Student Student // foreign key
+
 }
