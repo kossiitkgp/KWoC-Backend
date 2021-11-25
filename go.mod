82eeb4a17170fc1ae548b3934c89f0f60dd5c4f8
rajivharlalka <rajivharlalka009@gmail.com>
2021-11-26 01:07:15 +0530

    Added Supabase

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.4
 	github.com/jinzhu/gorm v1.9.14
+	gorm.io/driver/postgres v1.2.2 // indirect
 )
--- utils/database.go
+++ utils/database.go
@@ -2,46 +2,39 @@ package utils
 
 import (
 	"fmt"
-	// "os"
+	"kwoc20-backend/models"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	// _ "github.com/jinzhu/gorm/dialects/mysql" // MySQL Dialect
-	_ "github.com/jinzhu/gorm/dialects/sqlite" //sqlite for dev
-
-	"kwoc20-backend/models"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite" // sqlite for dev
 )
 
 // InitialMigration Initialize migration
 func InitialMigration() {
-	// DatabaseUsername := os.Getenv("DATABASE_USERNAME")
-	// DatabasePassword := os.Getenv("DATABASE_PASSWORD")
-	// DatabaseName := os.Getenv("DATABASE_NAME")
-	// DatabaseHost := os.Getenv("DATABASE_HOST")
-	// DatabasePort := os.Getenv("DATABASE_PORT")
+	DatabaseUsername := os.Getenv("DATABASE_USERNAME")
+	DatabasePassword := os.Getenv("DATABASE_PASSWORD")
+	DatabaseName := os.Getenv("DATABASE_NAME")
+	DatabaseHost := os.Getenv("DATABASE_HOST")
+	DatabasePort := os.Getenv("DATABASE_PORT")
 
-	// DatabaseURI := fmt.Sprintf(
-	// 	"%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-	// 	DatabaseUsername,
-	// 	DatabasePassword,
-	// 	DatabaseHost,
-	// 	DatabasePort,
-	// 	DatabaseName,
-	// )
-
-	// db, err := gorm.Open("mysql", DatabaseURI)
-	// if err != nil {
-	// 	LOG.Println(err)
-	// 	panic(err)
-	// }
-	// defer db.Close()
-
-	// temporary SQLite for ease of development
-	db, err := gorm.Open("sqlite3", "kwoc.db")
+	newURI := "host=" + DatabaseHost + " port=" + DatabasePort + " user=" + DatabaseUsername + " dbname=" + DatabaseName + " sslmode=disable password=" + DatabasePassword
+	fmt.Print(newURI)
+	db, err := gorm.Open("postgres", newURI)
 	if err != nil {
-		fmt.Println(err.Error())
-		panic("failed to connect database")
+		LOG.Println(err)
+		panic(err)
 	}
 	defer db.Close()
+
+	// temporary SQLite for ease of development
+	// db, err := gorm.Open("sqlite3", "kwoc.db")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	panic("failed to connect database")
+	// }
+	// defer db.Close()
 
 	db.AutoMigrate(&models.Mentor{})
 	db.AutoMigrate(&models.Student{})
@@ -81,5 +74,4 @@ func GetDB() *gorm.DB {
 	}
 
 	return db
-
 }
