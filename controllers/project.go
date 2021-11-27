2d881af96a193ca51291918f799411bd89c12886
rajivharlalka <rajivharlalka009@gmail.com>
2021-11-27 13:48:54 +0530

    Commented Code removed

--- controllers/project.go
+++ controllers/project.go
@@ -63,7 +63,6 @@ func ProjectReg(req map[string]interface{}, r *http.Request) (interface{}, int) 
 }
 
 // ProjectGet endpoint to fetch all projects
-// INCOMPLETE BECAUSE MENTOR STILL NEEDS TO BE ADDED
 func AllProjects(req map[string]interface{}, r *http.Request) (interface{}, int) {
 	db := utils.GetDB()
 	defer db.Close()
@@ -79,78 +78,6 @@ func AllProjects(req map[string]interface{}, r *http.Request) (interface{}, int)
 	fmt.Println(projects)
 
 	return projects, 200
-
-	// type project_details_struct struct {
-	// 	Name     string
-	// 	Desc     string
-	// 	Tags     string
-	// 	RepoLink string
-	// 	// ComChannel              string
-	// 	Branch string
-	// 	// README                  string
-	// 	// MentorName              string
-	// 	// MentorUsername          string
-	// 	// MentorEmail             string
-	// 	// SecondaryMentorName     string
-	// 	// SecondaryMentorUsername string
-	// 	SecondaryMentorEmail string
-	// }
-
-	// for _, v := range projects {
-	// 	type Response map[string]interface{}
-	// 	fmt.Print(project_details_struct{
-	// 		v.Name,
-	// 		v.Desc,
-	// 		v.Tags,
-	// 		v.Branch,
-	// 		v.RepoLink,
-	// 		v.SecondaryMentor.Email,
-	// 	})
-	// 	// projects = append(projects, res
-	// }
-
-	// var data []project_and_mentor
-	// for _, project := range projects {
-
-	// 	mentor_names := make([]string, 1)
-	// 	mentor_usernames := make([]string, 1)
-	// 	mentor_emails := make([]string, 1)
-
-	// 	var mentor models.Mentor
-	// 	var secondary_mentor models.Mentor
-
-	// 	var project_and_mentor_x project_and_mentor
-	// 	err := db.First(&mentor, project.MentorID).Error
-	// 	if err != nil {
-	// 		w.WriteHeader(http.StatusInternalServerError)
-	// 		return
-	// 	}
-	// 	mentor_names[0] = mentor.Name
-	// 	mentor_usernames[0] = mentor.Username
-	// 	mentor_emails[0] = mentor.Email
-
-	// 	if project.SecondaryMentorID != 0 {
-	// 		err := db.First(&secondary_mentor, project.SecondaryMentorID).Error
-	// 		if err != nil {
-	// 			w.WriteHeader(http.StatusInternalServerError)
-	// 			return
-	// 		}
-	// 		mentor_names = append(mentor_names, secondary_mentor.Name)
-	// 		mentor_usernames = append(mentor_usernames, secondary_mentor.Username)
-	// 		mentor_emails = append(mentor_emails, secondary_mentor.Email)
-	// 	}
-
-	// 	project_and_mentor_x.ProjectName = project.Name
-	// 	project_and_mentor_x.ProjectDesc = project.Desc
-	// 	project_and_mentor_x.ProjectTags = project.Tags
-	// 	project_and_mentor_x.ProjectRepoLink = project.RepoLink
-	// 	project_and_mentor_x.ProjectComChannel = project.ComChannel
-	// 	project_and_mentor_x.MentorName = mentor_names
-	// 	project_and_mentor_x.MentorUsername = mentor_usernames
-	// 	project_and_mentor_x.MentorEmail = mentor_emails
-
-	// 	data = append(data, project_and_mentor_x)
-	// }
 }
 
 // Run stats of all projects
@@ -171,8 +98,7 @@ func UpdateDetails(req map[string]interface{}, r *http.Request) (interface{}, in
 				"tags" : Updated tags of project,
 				"branch" : updated branch,
 				"readme" :  Project Readme,
-
-			
+				"secondaryMentor":Secondary Mentor Username
 			}
 	*/
 	db := utils.GetDB()
@@ -201,7 +127,7 @@ func UpdateDetails(req map[string]interface{}, r *http.Request) (interface{}, in
 	}
 
 	if projects.Mentor.Username != ctx_user {
-		fmt.Println(projects.Mentor.Username,"+", ctx_user)
+		fmt.Println(projects.Mentor.Username, "+", ctx_user)
 		return "Session Hijacking", 403
 	}
 
