b28f1de06999c053382ad409340fd9dad70ee147
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2022-11-13 19:59:05 +0530

    feat: use middleware to log all requests

--- utils/logger.go
+++ utils/logger.go
@@ -23,3 +23,29 @@ func Logger(inner http.Handler) http.Handler {
 		)
 	})
 }
+
+func GetNotFoundHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+
+		log.Warn().Msgf(
+			"Invalid Request: %s %s %s",
+			r.Method,
+			r.RequestURI,
+			"404 Path Not Found",
+		)
+	})
+}
+
+func GetMethodNotAllowedHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(405)
+
+		log.Warn().Msgf(
+			"Invalid Request: %s %s %s",
+			r.Method,
+			r.RequestURI,
+			"405 Method Not Allowed",
+		)
+	})
+}
