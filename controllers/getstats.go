97d94e96899a92bf8177f4fca60a5340e7bef8af
theredmac <102948598+theredmac@users.noreply.github.com>
2022-12-05 15:22:45 +0530

    Update getstats.go

--- controllers/getstats.go
+++ controllers/getstats.go
@@ -1,34 +1,58 @@
 package controllers
 
 import (
-	"encoding/json"
 	"net/http"
 
+	"kwoc20-backend/utils"
+
 	"github.com/gorilla/mux"
-	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
-var err error
+func CheckStudent(w http.ResponseWriter, r *http.Request) {
+	db := utils.GetDB()
+
+	params := mux.Vars(r)
+	var student Student
+	db.Find(&student, params["Username"])
+	if student == NULL {
+		w.WriteHeader(400)
+		w.Write([]byte("false"))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("true"))
+	}
+}
 
 func AllStudents(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	db := utils.GetDB()
+	w.WriteHeader(200)
 	var students []Student
-	DB.Find(&students)
-	json.NewEncoder(w).Encode(students)
+	db.Find(&students)
+	w.Write(students)
 }
 
 func OneStudent(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	db := utils.GetDB()
+	w.WriteHeader(200)
 	params := mux.Vars(r)
 	var student Student
-	DB.Find(&student, params["username"])
-	json.NewEncoder(w).Encode(student)
+	db.Find(&student, params["username"])
+	w.Write(student)
 }
 
 func AllProjects(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	db := utils.GetDB()
+	w.WriteHeader(200)
 	var projects []Project
-	DB.Find(&projects)
-	json.NewEncoder(w).Encode(projects)
+	db.Find(&projects)
+	w.Write(projects)
+}
+
+func OneMentor(w http.ResponseWriter, r *http.Request) {
+	db := utils.GetDB()
+	w.WriteHeader(200)
+	params := mux.Vars(r)
+	var mentor []Project
+	db.Find(&mentor, params["Mentor.Username"])
+	w.Write(mentor)
 }
