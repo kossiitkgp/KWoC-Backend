d89d4776038b8249ff8f6c50d27afd09a3036713
theredmac <102948598+theredmac@users.noreply.github.com>
2022-12-08 21:29:54 +0530

    Update getstats.go

--- controllers/getstats.go
+++ controllers/getstats.go
@@ -12,12 +12,29 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Student struct {
+	ID           uint
+	Username     string
+	CommitCount  uint
+	PRCount      uint
+	AddedLines   uint
+	RemovedLines uint
+	TechWorked   string
+}
+
 func CheckStudent(w http.ResponseWriter, r *http.Request) {
 	db := utils.GetDB()
-
 	params := mux.Vars(r)
-	var student models.Student
-	db.Find(&student, params["username"])
+	var student Student
+	db.
+	Table("students").
+	Select(
+		"id", "username", "commit_count",
+		"pr_count", "added_lines", "removed_lines",
+		"tech_worked",
+	).
+	Find(&student, params["username"])
+
 	if student.Username == " " {
 		w.WriteHeader(400)
 		_, err := w.Write([]byte("false"))
@@ -38,8 +55,15 @@ func CheckStudent(w http.ResponseWriter, r *http.Request) {
 func AllStudents(w http.ResponseWriter, r *http.Request) {
 	db := utils.GetDB()
 	w.WriteHeader(200)
-	var students []models.Student
-	db.Find(&students)
+	var students []Student
+	db.
+	Table("students").
+	Select(
+		"id", "username", "commit_count",
+		"pr_count", "added_lines", "removed_lines",
+		"tech_worked",
+	).
+	Find(&students)
 	str := fmt.Sprintf("%+v", students)
 	_, err := w.Write([]byte(str))
 	if err != nil {
@@ -52,8 +76,15 @@ func OneStudent(w http.ResponseWriter, r *http.Request) {
 	db := utils.GetDB()
 	w.WriteHeader(200)
 	params := mux.Vars(r)
-	var student models.Student
-	db.Find(&student, params["username"])
+	var student Student
+	db.
+	Table("students").
+	Select(
+		"id", "username", "commit_count",
+		"pr_count", "added_lines", "removed_lines",
+		"tech_worked",
+	).
+	Find(&student, params["username"])
 	str := fmt.Sprintf("%+v", student)
 	_, err := w.Write([]byte(str))
 	if err != nil {
@@ -62,11 +93,32 @@ func OneStudent(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+type Project struct {
+	ID uint
+	RepoLink      string
+	Branch        string
+
+	LastPullDate  string
+
+	CommitCount   uint
+	PRCount       uint
+	AddedLines    uint
+	RemovedLines  uint
+}
+
 func GetAllProjects(w http.ResponseWriter, r *http.Request) {
 	db := utils.GetDB()
 	w.WriteHeader(200)
-	var projects []models.Project
-	db.Find(&projects)
+	var projects []Project
+	db.
+		Table("projects").
+		Where("project_status = ?", "true").
+		Select(
+			"id", "repo_link", "branch",
+			"last_pull_date", "commit_count",
+			"pr_count", "added_lines", "removed_lines",
+		).
+		Find(&projects)
 	str := fmt.Sprintf("%+v", projects)
 	_, err := w.Write([]byte(str))
 	if err != nil {
@@ -80,7 +132,15 @@ func OneMentor(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	params := mux.Vars(r)
 	var mentor []models.Project
-	db.Find(&mentor, params["Mentor.username"])
+	db.
+		Table("projects").
+		Where("project_status = ?", "true").
+		Select(
+			"id", "repo_link", "branch",
+			"last_pull_date", "commit_count",
+			"pr_count", "added_lines", "removed_lines",
+		).
+		Find(&mentor, params["Mentor.username"])
 	str := fmt.Sprintf("%+v", mentor)
 	_, err := w.Write([]byte(str))
 	if err != nil {
