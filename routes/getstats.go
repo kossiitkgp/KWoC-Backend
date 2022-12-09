8848cdd3e7e2a111cff218c740e5dada51d23514
theredmac <102948598+theredmac@users.noreply.github.com>
2022-12-09 18:37:55 +0530

    Update getstats.go

--- routes/getstats.go
+++ routes/getstats.go
@@ -8,9 +8,9 @@ import (
 
 func RegisterGetStats(r *mux.Router) {
 
-	r.HandleFunc("/student/<username>", controllers.CheckStudent).Methods("GET")
+	r.HandleFunc("/student/exists/{username}", controllers.CheckStudent).Methods("GET")
 	r.HandleFunc("/students", controllers.AllStudents).Methods("GET")
-	r.HandleFunc("/student/<username>", controllers.OneStudent).Methods("GET")
+	r.HandleFunc("/student/{username}", controllers.OneStudent).Methods("GET")
 	r.HandleFunc("/projects", controllers.GetAllProjects).Methods("GET")
-	r.HandleFunc("/projects/<Mentor.Username>", controllers.OneMentor).Methods("GET")
+	r.HandleFunc("/projects/{Mentor.Username}", controllers.OneMentor).Methods("GET")
 }
