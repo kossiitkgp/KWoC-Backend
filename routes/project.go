313a0afe6a845c4389b33611b892386317980a2d
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2022-12-10 21:00:09 +0530

    feat: comment unused stats.go

--- routes/project.go
+++ routes/project.go
@@ -11,7 +11,7 @@ func RegisterProject(r *mux.Router) {
 	// Wrap the below Endpoint under LoginRequired Middleware after testing
 	r.HandleFunc("/add", utils.PermissiveCORSMiddleware(utils.LoginRequired(utils.JsonIO(controllers.ProjectReg)))).Methods("POST")
 	r.HandleFunc("", utils.PermissiveCORSMiddleware(utils.JsonIO(controllers.AllProjects))).Methods("GET")
-	r.HandleFunc("/stats", utils.PermissiveCORSMiddleware(utils.JsonIO(controllers.RunStats))).Methods("GET")
+	// r.HandleFunc("/stats", utils.PermissiveCORSMiddleware(utils.JsonIO(controllers.RunStats))).Methods("GET")
 	r.HandleFunc("/details", utils.PermissiveCORSMiddleware(utils.LoginRequired(utils.JsonIO(controllers.ProjectDetails)))).Methods("POST")
 	r.HandleFunc("/update", utils.PermissiveCORSMiddleware(utils.LoginRequired(utils.JsonIO(controllers.UpdateDetails)))).Methods("PUT")
 }
