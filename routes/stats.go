04996936523e5e286bd030272b2f6a4a4a4257b8
Rajiv Harlalka <rajivharlalka009@gmail.com>
2022-12-13 19:10:20 +0530

    changed stats POST route to GET (#93)
    Signed-off-by: rajivharlalka <rajivharlalka009@gmail.com>
    
    Signed-off-by: rajivharlalka <rajivharlalka009@gmail.com>

--- routes/stats.go
+++ routes/stats.go
@@ -11,10 +11,10 @@ func RegisterGetStats(r *mux.Router) {
 	r.HandleFunc("/student/exists/{username}", utils.PermissiveCORSMiddleware(utils.JsonIO(controllers.CheckStudent))).Methods("GET")
 
 	r.HandleFunc("/students", utils.PermissiveCORSMiddleware(utils.JsonIO(controllers.AllStudents))).Methods("GET")
-	r.HandleFunc("/student/{username}", utils.PermissiveCORSMiddleware(utils.LoginRequired(utils.JsonIO(controllers.OneStudent)))).Methods("POST")
+	r.HandleFunc("/student/{username}", utils.PermissiveCORSMiddleware(utils.LoginRequired(utils.JsonIO(controllers.OneStudent)))).Methods("GET")
 
 	r.HandleFunc("/projects", utils.PermissiveCORSMiddleware(utils.JsonIO(controllers.GetAllProjects))).Methods("GET")
-	r.HandleFunc("/mentor/{Mentor.Username}", utils.PermissiveCORSMiddleware(utils.LoginRequired(utils.JsonIO(controllers.OneMentor)))).Methods("POST")
+	r.HandleFunc("/mentor/{Mentor.Username}", utils.PermissiveCORSMiddleware(utils.LoginRequired(utils.JsonIO(controllers.OneMentor)))).Methods("GET")
 
 	r.HandleFunc("/overall", utils.PermissiveCORSMiddleware(utils.JsonIO(controllers.OverallStats))).Methods("GET")
 }
