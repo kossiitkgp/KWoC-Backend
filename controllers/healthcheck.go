b326735dc3d27c5b17c9f5e5b8565aabcdeef9ac
MateSousa <matessousa@outlook.com>
2023-06-18 11:14:08 -0300

    🎉 feat(healthcheck.go): add Ping and HealthCheck functions The Ping function responds with "pong" and returns the latency. The HealthCheck function checks the server and database status. If the database connection fails, it returns a 500 status code. If the ping to the database fails, it also returns a 500 status code. Otherwise, it returns a 200 status code and "OK".

---
+++ controllers/healthcheck.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"kwoc-backend/utils"
+	"net/http"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+// Ping responds with "pong" and returns the latency.
+func Ping(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("pong"))
+	if err != nil {
+		log.Err(err).Msg("Could not respond to Ping")
+	}
+
+	elapsed := time.Since(start)
+	log.Info().Str("latency", elapsed.String()).Msg("Ping request processed")
+}
+
+// HealthCheck checks the server and database status.
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	db, err := utils.GetDB()
+	if err != nil {
+		log.Err(err).Msg("Could not connect to database")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = db.Exec("SELECT 1").Error
+	if err != nil {
+		log.Err(err).Msg("Could not ping database")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write([]byte("OK"))
+}
