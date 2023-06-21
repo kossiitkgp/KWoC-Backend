e504cb88e3751a1d407d215fa5c4aa64c7967151
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-06-21 16:14:41 +0530

    refactor: create separate functions for mentor tests

--- controllers/common_test.go
+++ controllers/common_test.go
@@ -1,10 +1,15 @@
 package controllers_test
 
 import (
+	"fmt"
 	"kwoc-backend/server"
+	"kwoc-backend/utils"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+	"time"
 
 	"gorm.io/gorm"
 )
@@ -31,6 +36,35 @@ func expectResponseBodyToBe(t *testing.T, res *httptest.ResponseRecorder, expect
 	if resBody != expectedBody {
 		t.Errorf("Expected response `%s`. Got `%s`.", expectedBody, resBody)
 	}
+}
+
+func setTestDB() *gorm.DB {
+	os.Setenv("DEV", "true")
+	os.Setenv("DEV_DB_PATH", "testDB.db")
+	db, _ := utils.GetDB()
+	_ = utils.MigrateModels(db)
+
+	return db
+}
+
+func unsetTestDB() {
+	os.Unsetenv("DEV_DB_PATH")
+	os.Unsetenv("DEV")
+	os.Remove("testDB.db")
+}
+
+func setTestJwtSecretKey() {
+	rand.Seed(time.Now().UnixMilli())
+
+	os.Setenv("JWT_SECRET_KEY", fmt.Sprintf("testkey%d", rand.Int()))
+}
+
+func unsetTestJwtSecretKey() {
+	os.Unsetenv("JWT_SECRET_KEY")
+}
+
+func getTestUsername() string {
+	return fmt.Sprintf("testuser%d", rand.Int())
 }
 
 func testRequestNoAuth(t *testing.T, method string, path string) {
--- controllers/mentor_test.go
+++ controllers/mentor_test.go
@@ -3,44 +3,15 @@ package controllers_test
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"kwoc-backend/controllers"
 	"kwoc-backend/utils"
-	"math/rand"
 	"net/http"
-	"os"
 	"testing"
-	"time"
+
+	"gorm.io/gorm"
 )
 
-// Test unauthenticated request to /mentor/form/
-func TestMentorNoAuth(t *testing.T) {
-	testRequestNoAuth(t, "POST", "/mentor/form/")
-}
-
-// Test request to /mentor/form/ with invalid jwt
-func TestMentorInvalidAuth(t *testing.T) {
-	testRequestInvalidAuth(t, "POST", "/mentor/form/")
-}
-
-// Test request to /mentor/form/ with session hijacking attempt
-func TestMentorSessionHijacking(t *testing.T) {
-	// Generate a jwt secret key for testing
-	rand.Seed(time.Now().UnixMilli())
-
-	os.Setenv("JWT_SECRET_KEY", fmt.Sprintf("testkey%d", rand.Int()))
-
-	testLoginFields := utils.LoginJwtFields{
-		Username: "someuser",
-	}
-
-	someuserJwt, _ := utils.GenerateLoginJwtString(testLoginFields)
-
-	reqFields := controllers.RegisterMentorReqFields{
-		Username: "anotheruser",
-		Email:    "anotheruseremail@example.com",
-	}
-
+func createMentorRegRequest(reqFields *controllers.RegisterMentorReqFields) *http.Request {
 	reqBody, _ := json.Marshal(reqFields)
 
 	req, _ := http.NewRequest(
@@ -48,6 +19,32 @@ func TestMentorSessionHijacking(t *testing.T) {
 		"/mentor/form/",
 		bytes.NewReader(reqBody),
 	)
+
+	return req
+}
+
+// Test unauthenticated request to /mentor/form/
+func TestMentorRegNoAuth(t *testing.T) {
+	testRequestNoAuth(t, "POST", "/mentor/form/")
+}
+
+// Test request to /mentor/form/ with invalid jwt
+func TestMentorRegInvalidAuth(t *testing.T) {
+	testRequestInvalidAuth(t, "POST", "/mentor/form/")
+}
+
+// Test request to /mentor/form/ with session hijacking attempt
+func TestMentorRegSessionHijacking(t *testing.T) {
+	// Generate a jwt secret key for testing
+	setTestJwtSecretKey()
+
+	testLoginFields := utils.LoginJwtFields{Username: "someuser"}
+
+	someuserJwt, _ := utils.GenerateLoginJwtString(testLoginFields)
+
+	reqFields := controllers.RegisterMentorReqFields{Username: "anotheruser"}
+
+	req := createMentorRegRequest(&reqFields)
 	req.Header.Add("Bearer", someuserJwt)
 
 	res := executeRequest(req, nil)
@@ -56,62 +53,71 @@ func TestMentorSessionHijacking(t *testing.T) {
 	expectResponseBodyToBe(t, res, "Login username and given username do not match.")
 }
 
-// Test a request to /mentor/form/ with proper authentication and input
-func TestMentorOK(t *testing.T) {
-	// Set up a local test database path
-	os.Setenv("DEV", "true")
-	os.Setenv("DEV_DB_PATH", "testDB.db")
-	db, _ := utils.GetDB()
-	_ = utils.MigrateModels(db)
-	// Remove the test database once used
-	defer os.Unsetenv("DEV_DB_PATH")
-	defer os.Remove("testDB.db")
-
-	// Generate a jwt secret key for testing
-	rand.Seed(time.Now().UnixMilli())
-
-	os.Setenv("JWT_SECRET_KEY", fmt.Sprintf("testkey%d", rand.Int()))
-
+// Test a new user registration request to /mentor/form/ with proper authentication and input
+func tMentorRegNewUser(db *gorm.DB, t *testing.T) {
 	// Test login fields
-	testUsername := fmt.Sprintf("testuser%d", rand.Int())
-	testLoginFields := utils.LoginJwtFields{
-		Username: testUsername,
-	}
+	testUsername := getTestUsername()
+	testLoginFields := utils.LoginJwtFields{Username: testUsername}
 
 	testJwt, _ := utils.GenerateLoginJwtString(testLoginFields)
+	reqFields := controllers.RegisterMentorReqFields{Username: testUsername}
 
-	reqFields := controllers.RegisterMentorReqFields{
-		Username: testUsername,
-		Email:    "testuser@example.com",
-	}
-
-	reqBody, _ := json.Marshal(reqFields)
-
-	// --- TEST NEW USER REGISTRATION ---
-	req, _ := http.NewRequest(
-		"POST",
-		"/mentor/form/",
-		bytes.NewReader(reqBody),
-	)
+	req := createMentorRegRequest(&reqFields)
 	req.Header.Add("Bearer", testJwt)
 
 	res := executeRequest(req, db)
 
 	expectStatusCodeToBe(t, res, http.StatusOK)
 	expectResponseBodyToBe(t, res, "Mentor registration successful.")
-	// --- TEST NEW USER REGISTRATION ---
+}
 
-	// --- TEST EXISTING USER REQUEST ---
-	req, _ = http.NewRequest(
-		"POST",
-		"/mentor/form/",
-		bytes.NewReader(reqBody),
-	)
+// Test an existing user registration request to /mentor/form/ with proper authentication and input
+func tMentorRegExistingUser(db *gorm.DB, t *testing.T) {
+	// Test login fields
+	testUsername := getTestUsername()
+	testLoginFields := utils.LoginJwtFields{Username: testUsername}
+
+	testJwt, _ := utils.GenerateLoginJwtString(testLoginFields)
+	reqFields := controllers.RegisterMentorReqFields{Username: testUsername}
+
+	req := createMentorRegRequest(&reqFields)
 	req.Header.Add("Bearer", testJwt)
 
-	res = executeRequest(req, db)
+	_ = executeRequest(req, db)
+
+	// Execute the same request again
+	req = createMentorRegRequest(&reqFields)
+	req.Header.Add("Bearer", testJwt)
+
+	res := executeRequest(req, db)
 
 	expectStatusCodeToBe(t, res, http.StatusBadRequest)
 	expectResponseBodyToBe(t, res, "Error: Mentor already exists.")
-	// --- TEST EXISTING USER REQUEST ---
+}
+
+// Test requests to /mentor/form/ with proper authentication and input
+func TestMentorRegOK(t *testing.T) {
+	// Set up a local test database path
+	db := setTestDB()
+	defer unsetTestDB()
+
+	// Generate a jwt secret key for testing
+	setTestJwtSecretKey()
+	defer unsetTestJwtSecretKey()
+
+	// New mentor registration test
+	t.Run(
+		"Test: new mentor registration.",
+		func(t *testing.T) {
+			tMentorRegNewUser(db, t)
+		},
+	)
+
+	// Existing mentor registration test
+	t.Run(
+		"Test: existing mentor registration.",
+		func(t *testing.T) {
+			tMentorRegExistingUser(db, t)
+		},
+	)
 }
