beef56c2f6194696eed80b876061291c2dfd7c8e
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-06-21 16:18:52 +0530

    refactor: split student endpoint tests

--- controllers/student_test.go
+++ controllers/student_test.go
@@ -6,42 +6,13 @@ import (
 	"fmt"
 	"kwoc-backend/controllers"
 	"kwoc-backend/utils"
-	"math/rand"
 	"net/http"
-	"os"
 	"testing"
-	"time"
+
+	"gorm.io/gorm"
 )
 
-// Test unauthenticated request to /student/form/
-func TestStudentNoAuth(t *testing.T) {
-	testRequestNoAuth(t, "POST", "/student/form/")
-}
-
-// Test request to /student/form/ with invalid jwt
-func TestStudentInvalidAuth(t *testing.T) {
-	testRequestInvalidAuth(t, "POST", "/student/form/")
-}
-
-// Test request to /student/form/ with session hijacking attempt
-func TestStudentSessionHijacking(t *testing.T) {
-	// Generate a jwt secret key for testing
-	rand.Seed(time.Now().UnixMilli())
-
-	os.Setenv("JWT_SECRET_KEY", fmt.Sprintf("testkey%d", rand.Int()))
-
-	testLoginFields := utils.LoginJwtFields{
-		Username: "someuser",
-	}
-
-	someuserJwt, _ := utils.GenerateLoginJwtString(testLoginFields)
-
-	reqFields := controllers.RegisterStudentReqFields{
-		Username: "anotheruser",
-		Email:    "anotheruseremail@example.com",
-		College:  "anotherusercollege",
-	}
-
+func createStudentRegRequest(reqFields *controllers.RegisterStudentReqFields) *http.Request {
 	reqBody, _ := json.Marshal(reqFields)
 
 	req, _ := http.NewRequest(
@@ -49,6 +20,32 @@ func TestStudentSessionHijacking(t *testing.T) {
 		"/student/form/",
 		bytes.NewReader(reqBody),
 	)
+
+	return req
+}
+
+// Test unauthenticated request to /student/form/
+func TestStudentRegNoAuth(t *testing.T) {
+	testRequestNoAuth(t, "POST", "/student/form/")
+}
+
+// Test request to /student/form/ with invalid jwt
+func TestStudentRegInvalidAuth(t *testing.T) {
+	testRequestInvalidAuth(t, "POST", "/student/form/")
+}
+
+// Test request to /student/form/ with session hijacking attempt
+func TestStudentRegSessionHijacking(t *testing.T) {
+	// Generate a jwt secret key for testing
+	setTestJwtSecretKey()
+
+	testLoginFields := utils.LoginJwtFields{Username: "someuser"}
+
+	someuserJwt, _ := utils.GenerateLoginJwtString(testLoginFields)
+
+	reqFields := controllers.RegisterStudentReqFields{Username: "anotheruser"}
+
+	req := createStudentRegRequest(&reqFields)
 	req.Header.Add("Bearer", someuserJwt)
 
 	res := executeRequest(req, nil)
@@ -57,63 +54,71 @@ func TestStudentSessionHijacking(t *testing.T) {
 	expectResponseBodyToBe(t, res, "Login username and given username do not match.")
 }
 
-// Test a request to /student/form/ with proper authentication and input
-func TestStudentOK(t *testing.T) {
-	// Set up a local test database path
-	os.Setenv("DEV", "true")
-	os.Setenv("DEV_DB_PATH", "testDB.db")
-	db, _ := utils.GetDB()
-	_ = utils.MigrateModels(db)
-	// Remove the test database once used
-	defer os.Unsetenv("DEV_DB_PATH")
-	defer os.Remove("testDB.db")
-
-	// Generate a jwt secret key for testing
-	rand.Seed(time.Now().UnixMilli())
-
-	os.Setenv("JWT_SECRET_KEY", fmt.Sprintf("testkey%d", rand.Int()))
-
+// Test a new user registration request to /student/form/ with proper authentication and input
+func tStudentRegNewUser(db *gorm.DB, t *testing.T) {
 	// Test login fields
-	testUsername := fmt.Sprintf("testuser%d", rand.Int())
-	testLoginFields := utils.LoginJwtFields{
-		Username: testUsername,
-	}
+	testUsername := getTestUsername()
+	testLoginFields := utils.LoginJwtFields{Username: testUsername}
 
 	testJwt, _ := utils.GenerateLoginJwtString(testLoginFields)
+	reqFields := controllers.RegisterStudentReqFields{Username: testUsername}
 
-	reqFields := controllers.RegisterStudentReqFields{
-		Username: testUsername,
-		Email:    "testuser@example.com",
-		College:  "testusercollege",
-	}
-
-	reqBody, _ := json.Marshal(reqFields)
-
-	// --- TEST NEW USER REGISTRATION ---
-	req, _ := http.NewRequest(
-		"POST",
-		"/student/form/",
-		bytes.NewReader(reqBody),
-	)
+	req := createStudentRegRequest(&reqFields)
 	req.Header.Add("Bearer", testJwt)
 
 	res := executeRequest(req, db)
 
 	expectStatusCodeToBe(t, res, http.StatusOK)
 	expectResponseBodyToBe(t, res, "Student registration successful.")
-	// --- TEST NEW USER REGISTRATION ---
+}
 
-	// --- TEST EXISTING USER REQUEST ---
-	req, _ = http.NewRequest(
-		"POST",
-		"/student/form/",
-		bytes.NewReader(reqBody),
-	)
+// Test an existing user registration request to /student/form/ with proper authentication and input
+func tStudentRegExistingUser(db *gorm.DB, t *testing.T) {
+	// Test login fields
+	testUsername := getTestUsername()
+	testLoginFields := utils.LoginJwtFields{Username: testUsername}
+
+	testJwt, _ := utils.GenerateLoginJwtString(testLoginFields)
+	reqFields := controllers.RegisterStudentReqFields{Username: testUsername}
+
+	req := createStudentRegRequest(&reqFields)
 	req.Header.Add("Bearer", testJwt)
 
-	res = executeRequest(req, db)
+	_ = executeRequest(req, db)
+
+	// Execute the same request again
+	req = createStudentRegRequest(&reqFields)
+	req.Header.Add("Bearer", testJwt)
+
+	res := executeRequest(req, db)
 
 	expectStatusCodeToBe(t, res, http.StatusBadRequest)
 	expectResponseBodyToBe(t, res, fmt.Sprintf("Student `%s` already exists.", testUsername))
-	// --- TEST EXISTING USER REQUEST ---
+}
+
+// Test requests to /student/form/ with proper authentication and input
+func TestStudentRegOK(t *testing.T) {
+	// Set up a local test database path
+	db := setTestDB()
+	defer unsetTestDB()
+
+	// Generate a jwt secret key for testing
+	setTestJwtSecretKey()
+	defer unsetTestJwtSecretKey()
+
+	// New student registration test
+	t.Run(
+		"Test: new student registration.",
+		func(t *testing.T) {
+			tStudentRegNewUser(db, t)
+		},
+	)
+
+	// Existing student registration test
+	t.Run(
+		"Test: existing student registration.",
+		func(t *testing.T) {
+			tStudentRegExistingUser(db, t)
+		},
+	)
 }
