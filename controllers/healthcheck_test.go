4c5b9c19ed05b6c7f57764105e2d81638c9618a6
ashwinpra <ashprasanth2003@gmail.com>
2023-06-25 19:59:50 +0530

    added healthcheck test script

---
+++ controllers/healthcheck_test.go
@@ -0,0 +1,25 @@
+package controllers_test
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/healthcheck/ping/", nil)
+	res := executeRequest(req, nil)
+
+	expectStatusCodeToBe(t, res, http.StatusOK)
+	expectResponseBodyToBe(t, res, "pong")
+}
+
+func TestHealthCheck(t *testing.T) {
+	db := setTestDB()
+	defer unsetTestDB()
+
+	req, _ := http.NewRequest("GET", "/healthcheck/", nil)
+	res := executeRequest(req, db)
+
+	expectStatusCodeToBe(t, res, http.StatusOK)
+	expectResponseBodyToBe(t, res, "OK")
+}
