3e1c0d30f358e5d2f2c84bf09e89b343589c19d6
Alph3ga <akashneelrc@gmail.com>
2023-06-29 03:09:36 +0530

    changed order of hook commands

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,11 +2,11 @@
 
 echo "Pre-commit hook running"
 
-echo "Running golangci-lint..."
-golangci-lint run
-
 echo "Running go mod tidy..."
 go mod tidy
+
+echo "Running golangci-lint..."
+golangci-lint run
 
 echo "Running go vet..."
 go vet ./...
