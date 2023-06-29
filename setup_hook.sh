fa29db0703f21b5047df5781af1939489d2b17b5
Alph3ga <akashneelrc@gmail.com>
2023-06-29 14:39:51 +0530

    GIT_DIR used in place of .git

--- setup_hook.sh
+++ setup_hook.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 SOURCE_FILE="pre-commit.sh"
-TARGET_FILE=".git/hooks/pre-commit"
+TARGET_FILE="${GIT_DIR:-".git"}/hooks/pre-commit"
 
-cp "$(dirname "$0")/$SOURCE_FILE" "$(dirname "$0")/$TARGET_FILE"
+cp "$(dirname "$0")/$SOURCE_FILE" "$TARGET_FILE"
+echo "Hook set up at $TARGET_FILE"
 chmod +x .git/hooks/pre-commit
