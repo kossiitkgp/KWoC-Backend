7741ae1d506995dc92fb42a710b7ea49321560f8
Alph3ga <akashneelrc@gmail.com>
2023-06-29 15:00:39 +0530

    removed unnecessary files

--- setup_hook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SOURCE_FILE="pre-commit.sh"
-TARGET_FILE="${GIT_DIR:-".git"}/hooks/pre-commit"
-
-cp "$(dirname "$0")/$SOURCE_FILE" "$TARGET_FILE"
-echo "Hook set up at $TARGET_FILE"
-chmod +x .git/hooks/pre-commit
