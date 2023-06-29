f1b1f44b9941e0b2e9a83787e1fc23f9ae95b3ef
Alph3ga <akashneelrc@gmail.com>
2023-06-29 13:52:44 +0530

    added check for golangci-lint existing

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+set -eu -o pipefail
+
 echo "Pre-commit hook running"
 
 echo "Running go mod tidy..."
 go mod tidy
 
+if ! command -v golangci-lint &> /dev/null ; then
+    echo "golangci-lint not installed or available in the PATH" >&2
+    echo "please check https://github.com/golangci/golangci-lint" >&2
+    exit 1
+fi
 echo "Running golangci-lint..."
 golangci-lint run
 
