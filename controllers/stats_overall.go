7731cf6c150d81a19190b8827eab625ed00d166c
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-03 16:35:06 +0530

    feat: added fetch overall stats endpoint

---
+++ controllers/stats_overall.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"kwoc-backend/middleware"
+	"kwoc-backend/utils"
+	"net/http"
+)
+
+type OverallStats struct {
+	TotalCommitCount  uint `json:"total_commit_count"`
+	TotalPullCount    uint `json:"total_pull_count"`
+	TotalLinesAdded   uint `json:"total_lines_added"`
+	TotalLinesRemoved uint `json:"total_lines_removed"`
+
+	GenTime int64 `json:"gen_time"`
+}
+
+func FetchOverallStats(w http.ResponseWriter, r *http.Request) {
+	app := r.Context().Value(middleware.APP_CTX_KEY).(*middleware.App)
+	db := app.Db
+
+	var overall_stats []OverallStats
+
+	tx := db.
+		Table("stats").
+		Order("gen_time DESC").
+		Find(&overall_stats)
+
+	if tx.Error != nil {
+		utils.LogErrAndRespond(
+			r,
+			w,
+			tx.Error,
+			"Error fetching stats from the database.",
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	utils.RespondWithJson(r, w, overall_stats)
+}
