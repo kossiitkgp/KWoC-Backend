a73f738589b906c614c4792a33e1d9310a8264e5
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-07-03 19:58:01 +0530

    feat: added fetch all project stats endpoint

---
+++ controllers/stats_project.go
@@ -0,0 +1,42 @@
+package controllers
+
+import (
+	"kwoc-backend/middleware"
+	"kwoc-backend/utils"
+	"net/http"
+)
+
+type ProjectStats struct {
+	Name     string `json:"name"`
+	RepoLink string `json:"repo_link"`
+
+	CommitCount  uint `json:"commit_count"`
+	PullCount    uint `json:"pull_count"`
+	LinesAdded   uint `json:"lines_added"`
+	LinesRemoved uint `json:"lines_removed"`
+}
+
+func FetchAllProjectStats(w http.ResponseWriter, r *http.Request) {
+	app := r.Context().Value(middleware.APP_CTX_KEY).(*middleware.App)
+	db := app.Db
+
+	var project_stats []ProjectStats
+
+	tx := db.
+		Table("projects").
+		Select("name", "repo_link", "commit_count", "pull_count", "lines_added", "lines_removed").
+		Find(&project_stats)
+
+	if tx.Error != nil {
+		utils.LogErrAndRespond(
+			r,
+			w,
+			tx.Error,
+			"Error fetching project stats from the database.",
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	utils.RespondWithJson(r, w, project_stats)
+}
