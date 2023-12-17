4681446c22ac70cead8eb7d5b3fb14a47b94f8b2
Devansh-bit <devanshg308@gmail.com>
2023-12-17 09:27:43 +0530

    docs: Add docs for Index

--- controllers/index.go
+++ controllers/index.go
@@ -5,7 +5,14 @@ import (
 	"fmt"
 	"net/http"
 )
-
+// Index godoc
+//
+//	@Summary		Checks api health
+//	@Description	Returns a simple string to check if the api is up and running
+//	@Accept			plain
+//	@Produce		plain
+//	@Success		200	{string}	string	"Hello from KOSS Backend!"
+//	@Router			/api [get]
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from KOSS Backend!")
 }
